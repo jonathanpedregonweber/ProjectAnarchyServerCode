80e02ce3c6819655dbacc80ee4d05a121e7bc580
jonathanpedregon <jonathanpedregon@gmail.com>
2018-08-07 20:27:30 -0600

    Adding methods and streams to ServerHandler. Awaiting MessageFactory to handle actual sending of messages.

--- src/Main/Handlers/ServerHandler.java
+++ src/Main/Handlers/ServerHandler.java
@@ -1,5 +1,50 @@
 package Main.Handlers;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class ServerHandler
 {
+    public PrintWriter Writer;
+    public BufferedReader Reader;
+
+    public ServerHandler(Socket socket)
+    {
+        try
+        {
+            Reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            Writer = new PrintWriter(socket.getOutputStream());
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void SendChatMessage(String chatMessage)
+    {
+
+    }
+
+    public void SendHitMessage(boolean hit)
+    {
+
+    }
+
+    public void SendMoveMessage(int xCoordinate, int yCoordinate)
+    {
+
+    }
+
+    public void SendStartMessage()
+    {
+
+    }
+
+    public void WinMessage()
+    {
+
+    }
 }
