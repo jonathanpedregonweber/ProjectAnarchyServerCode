1c5afb065af9e921d9491dc6c1e11b6666a1fc46
jonathanpedregon <jonathanpedregon@gmail.com>
2018-08-09 20:05:42 -0600

    Main method now holds an example of a connection to the server. Real location and port are being used.

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,8 +1,25 @@
 package Main;
 
+import Main.Handlers.ServerHandler;
+import Main.Models.HitMessage;
+import Main.Models.MessageFactory;
+
+import java.io.IOException;
+import java.net.Socket;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+        String serverLocation = "ec2-18-207-150-67.compute-1.amazonaws.com";
+        int port =  8989;
+        try
+        {
+            Socket socket = new Socket(serverLocation, port);
+            ServerHandler handler = new ServerHandler(socket);
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
